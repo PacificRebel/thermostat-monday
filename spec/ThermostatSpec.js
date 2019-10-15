32767b466232d4a85e57b5d97048d95cf31c3092
Heli Sivunen <heli@mooncat.tv>
2019-10-15 14:33:44 +0100

    Add powersave function with maximum temp of 25 and overall maximum of 32.

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -4,7 +4,7 @@ describe('Thermostat', function() {
   var thermostat;
 
   beforeEach(function() {
-    thermostat = new Thermostat(); 
+    thermostat = new Thermostat();
   });
 
   it('starts at 20 degrees', function() {
@@ -25,9 +25,28 @@ describe('Thermostat', function() {
     for (var i = 0; i < 11; i++) {
       thermostat.down();
     }
-
     expect(thermostat.getCurrentTemp()).toEqual(10);
-
   });
 
+  it('if power saving mode is on, maximum temp is 25', function () {
+    thermostat.powerSaveOn();
+    expect(thermostat.maximumTemp).toEqual(25)
+  });
+
+  it('if power saving mode is on by default, maximum temp is 25', function () {
+    expect(thermostat.maximumTemp).toEqual(25)
+  });
+
+  it('if power saving mode is off, maximum temp is 32', function () {
+    thermostat.powerSaveOff();
+    expect(thermostat.maximumTemp).toEqual(32)
+  });
+
+   it('user cannot increase temperature beyond 32', function () {
+     thermostat.powerSaveOff();
+     for (var i = 20; i < 33; i++) {
+       thermostat.up();
+     }
+    expect(thermostat.getCurrentTemp()).toEqual(32)
+   })
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -4,13 +4,17 @@
 function Thermostat () {
   this.MINIMUM_TEMP = 10;
   this._temp = 20;
-}
+  this.maximumTemp = 25;
+};
 
 Thermostat.prototype.getCurrentTemp = function() {
   return this._temp;
 };
 
 Thermostat.prototype.up = function() {
+  if  (this.isMaximumTemp()) {
+    return;
+  }
   this._temp += 1;
 };
 
@@ -23,4 +27,16 @@ Thermostat.prototype.down = function() {
 
 Thermostat.prototype.isMinimumTemp = function() {
   return this._temp === this.MINIMUM_TEMP;
+};
+
+Thermostat.prototype.isMaximumTemp = function() {
+  return this._temp === this.maximumTemp;
+}
+
+Thermostat.prototype.powerSaveOn = function() {
+  this.maximumTemp = 25;
+};
+
+Thermostat.prototype.powerSaveOff = function() {
+  this.maximumTemp = 32;
 };
