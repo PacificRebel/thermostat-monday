acfd332aec6811c5bb23ed7464f93c82ef0e4372
Heli Sivunen <heli@mooncat.tv>
2019-10-15 16:34:16 +0100

    Add HTML code and bits of jQuery

---
+++ src/interface.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//   var thermostat = new Thermostat();
+//   updateTemperature();
+//
+//   $('#increase').click(function(){
+//     thermostat.up();
+//     updateTemperature();
+//   })
+//
+//   function updateTemperature() {
+//     $('h1').text(thermostat.getCurrentTemp);
+//   });
+// });
